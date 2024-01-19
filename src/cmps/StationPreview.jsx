9b77fbd43da152df85d3f7152c1ee26edb6b67e7
codingkinesis <ilanbarcohn@gmail.com>
2024-01-19 11:23:06 +0200

    and onclick station preview

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router';
 
 export const StationPreview = ({ station }) => {
   if (!station) return null; // Add a check for null or undefined station
 
+  const navigate = useNavigate();
+
+  console.log(station)
+
   return (
     <div className="station-preview">
       <div className="image-container">
-        <img src={station.createdBy.imgUrl} alt={station.name} />
-        <button className="play-btn" onClick={() => {}}>
+        <img src={station.imgUrl} alt={station.name} />
+        <button className="play-btn" onClick={() => {navigate(`/melodiva/station/${station._id}`, { replace: true })}}>
           <FontAwesomeIcon icon={faPlayCircle} /> {/* Use Font Awesome icon */}
         </button>
       </div>
