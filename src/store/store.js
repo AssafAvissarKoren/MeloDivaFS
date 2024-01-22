a1f2d7dea51a0a1eba9a64a29f2b547ac5f539dd
codingkinesis <ilanbarcohn@gmail.com>
2024-01-22 18:09:06 +0200

    remove track from store

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,11 @@ import { combineReducers, compose, legacy_createStore as createStore } from 'red
 import { robotReducer } from './reducers/robot.reducer'
 import { userReducer } from './reducers/user.reducer'
 import { stationReducer } from './reducers/station.reducer'
-import { trackReducer } from './reducers/track.reducer'
 
 const rootReducer = combineReducers({
     robotModule: robotReducer,
     userModule: userReducer,
     stationModule: stationReducer,
-    trackModule: trackReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
