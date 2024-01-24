81e18bfce312e8360ababb183ea5034850178885
codingkinesis <ilanbarcohn@gmail.com>
2024-01-24 13:53:29 +0200

    add queqe to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, compose, legacy_createStore as createStore } from 'redux'
 import { stationReducer } from './reducers/station.reducer'
 import { categoryReducer } from './reducers/category.reducer'
+import { queqeReducer } from './reducers/queqe.reducer'
 
 const rootReducer = combineReducers({
     stationModule: stationReducer,
     categoryModule: categoryReducer,
+    queqeModule: queqeReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
