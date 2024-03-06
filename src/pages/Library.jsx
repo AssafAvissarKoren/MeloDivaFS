99433d233c5e35743f03ae02ad9ea37f8a97bd2c
codingkinesis <ilanbarcohn@gmail.com>
2024-03-06 15:47:16 +0200

    pixelperfect library

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -6,9 +6,8 @@ import { saveStation } from "../store/actions/station.actions"
 import { utilService } from "../services/util.service"
 import { IndexContext } from '../cmps/IndexContext.jsx'
 import { useContext, useEffect } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook, faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBook, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export function Library() {
     const { setFilterBy } = useContext(IndexContext)
@@ -45,7 +44,7 @@ export function Library() {
         <div className="library">
             <div className="title">
                 <FontAwesomeIcon icon={faBook} className="symbol" aria-hidden="true" />
-                <p>Library</p>
+                <p>Your Library</p>
                 <FontAwesomeIcon icon={faPlus} className="symbol add-station-btn" aria-hidden="true" onClick={createNewStation}/>
             </div>
             <ul className="content">
